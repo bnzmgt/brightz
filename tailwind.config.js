dc6c9490bcbb67a6dcaf2f2dbc828d4fdeb0e17f
wickborlan <wickborlan@gmail.com>
2024-12-06 08:45:01 +0700

    add google fonts dynamic #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-content: ["./**/*.php"],
+    purge: {
+        content: ['./**/*.php', './**/*.html', './src/**/*.js'],
+        options: {
+            safelist: ['dynamic-font'], // Add your dynamic classes here
+        },
+    },
   theme: {
     
     extend: {
         fontFamily: {
-            sans: ['Outfit', 'sans-serif'],
+            //sans: ['Outfit', 'sans-serif'],
+            sans: ['Roboto', ...defaultTheme.fontFamily.sans],
         },
         colors: {
             'primary': '#182955',
